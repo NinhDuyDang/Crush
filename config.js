361f724951f10ea18a8eb9f1e9e89fa4475bffef
Ninh Dang <Ninhduy2000@gmail.com>
2024-07-30 11:23:13 +0700

    NinhDang

---
+++ config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+    titleWeb: "Love Hoàng Linh",
+    introTitle: 'Trà đào cam xả xinh đẹp',
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy anh ',
+    btnYes: 'Yêu <33',
+    btnNo: 'Không yêu',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh ( Ninh Đăng ) ',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu a Ninh Đăng ',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://l.facebook.com/l.php?u=https%3A%2F%2Fm.me%2Fninh.duy.90281%3Ffbclid%3DIwZXh0bgNhZW0CMTAAAR1mk-MNGToxwkGGwkOvu9Tu_aRM-yRZsqBEE0MoSYzKlSK51K6VGOc7Jsg_aem_50JA9Yn2_K1LV2Z5pyASiw&h=AT0-fe1GI6uPg1yEaSBkHYnJ5NZ-AJ_VflOpvyFUCa7oaA3Cn-pxcefdnLYOFtizmU8c8YxP1B76BntjfmLmxAgXBsQWEI0AMGfVom2W4sHRhUo44IO1Dj_BecPbbWPEg7r25g'
+}
